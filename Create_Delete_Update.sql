daf6e4b760be618ec81339abd868a9bd58f66356
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-08 09:19:44 +0600

    Create Create_Delete_Update.sql

---
+++ Create_Delete_Update.sql
@@ -0,0 +1,21 @@
+-- Dropping tables if they exist
+DROP TABLE IF EXISTS Student;
+-- Creating Student table
+CREATE TABLE Student(
+    ID CHAR(4) PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks DOUBLE
+);
+
+-- Inserting a row into Student
+INSERT INTO Student (ID, Name, Marks) 
+VALUES ('1', 'ABC', 90);
+
+-- Updating the Name of the student with ID = 1
+UPDATE Student
+SET Name = 'Mst. ABC'
+WHERE ID = '1';  -- Use primary key to update the specific row
+
+-- Deleting the student with ID = 1
+DELETE FROM Student
+WHERE ID = '1';  -- Use primary key to delete the specific row
