795bf920fb6b486352ce37aede6706a7bc7ad8fb
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-12 11:16:19 +0600

    Update Employee_details.sql

--- Employee_details.sql
+++ Employee_details.sql
@@ -7,12 +7,12 @@ Create Table Employee
 (
     EmployeeID int Not Null Primary key,
     FirstName Varchar(20) Not Null,
-    SecondName Varchar(30) Not Null,
+    LastName Varchar(30) Not Null,
     Age int Not Null,
     Department Varchar(20) Not Null
 );
 
-Insert into Employee(EmployeeID, FirstName, SecondName, Age, Department)
+Insert into Employee(EmployeeID, FirstName, LastName, Age, Department)
 Values
 (1, 'John', 'Doe', 28, 'Sales'),
 (2, 'Jane', 'Smith', 32, 'Marketing'),
@@ -28,8 +28,21 @@ Values
 
 
 
-Select Name
+Select Distinct Department
+From Employee;
+
+Select LastName, Age
 From Employee
+Order by Age DESC;
 
+Select LastName, Age
+From Employee
+Where Age > 30 AND Department = 'Marketing';
 
+Select * 
+From Employee;
+
+Select *
+From Employee
+Where Department = 'Engineering';
 
